ba7377373d300d4f0b3afa67cac83a5892f3e5f4
Thor Kell <thor@tide-pool.ca>
2016-05-22 18:26:32 -0400

    a real route

--- server.py
+++ server.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python
 # encoding: utf=8
 
-from google import search
+from flask import Flask
+from flask import request
+from google import search as google_search
 
-store_strings = ['bandcamp', 'boomkat', 'bleep', 'itunes']
-artist = 'Archie Pelago'
-title = 'Clammy Customer'
+STORE_STRINGS = ['bandcamp', 'boomkat', 'bleep', 'itunes']
+app = Flask(__name__)
 
-urls = []
-for store_string in store_strings:
-    search_string = '%s %s %s' % (artist, title, store_string)
-    res = search(search_string)
-    urls.append((store_string, res.next()))
+@app.route('/search')
+def search():
+    '''
+    Takes a title and artist param, and searches for artist - title <store>
+    '''
+    artist = request.args.get('artist', '')
+    title = request.args.get('title', '')
+    urls = []
+    for store_string in STORE_STRINGS:
+        search_string = '%s - %s %s' % (artist, title, store_string)
+        res = google_search(search_string)
+        urls.append((store_string, res.next()))
+
+    return urls
 

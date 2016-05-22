30c272aa12e6c10bc28f0cf72f77c4325c9dbbdb
Thor Kell <thor@tide-pool.ca>
2016-05-22 19:09:45 -0400

    working on the js

---
+++ index.js
@@ -0,0 +1,9 @@
+
+var server = 'http://wombat.com'
+var getUrls = function() {
+    var artist = $('#artist').val();
+    var title = $('#title').val();
+    var url = server + '?artist=' + artist + '&title=' + title;
+    // need a get here, and a real server name
+}
+
